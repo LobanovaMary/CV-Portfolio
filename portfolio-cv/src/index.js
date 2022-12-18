91d1fc26116e383a178135842a71201bf3f4ec9c
Lobanova Mary <lobanovama1@gmail.com>
2022-12-18 21:06:59 +0300

    feat: add burger menu

--- portfolio-cv/src/index.js
+++ portfolio-cv/src/index.js
@@ -2,9 +2,11 @@ import './style/main.scss';
 
 import postForm from './scripts/post-form.js';
 import renderWorkItems from './scripts/render-work-items.js';
+import burgerMenu from './scripts/burgerMenu.js';
 
 postForm();
 renderWorkItems();
+burgerMenu();
 
 const nextWorkItem = document.querySelector('.work-description__btn-next');
 const prevWorkItem = document.querySelector('.work-description__btn-prev');
